555c21433f5edf2afe8b11fc79241dbbcb1d2c8b
CodEros9231 <84926496+CodEros9231@users.noreply.github.com>
2021-05-29 15:00:02 +0400

    Starting Try for the temperature task. major challenge to fill the matrix with numbers
    Major challenge to display numbers in the M5 stack atom, used .c file created by .exe app from other git repository
    https://github.com/m5stack/M5Atom
    
    The pattern.c file is for a random pattern within 5*5 matrix

--- Temperature_Task/Temperature_Task.ino
+++ Temperature_Task/Temperature_Task.ino
@@ -1,9 +1,15 @@
+#include "M5Atom.h"
+bool IMU6886Flag = false;
+
 void setup() {
   // put your setup code here, to run once:
-
+M5.begin(true,false,true);
+delay(20);
 }
 
 void loop() {
+  M5.dis.drawpix(0,0,0xffffff);
+  M5.update();
   // put your main code here, to run repeatedly:
 
 }
