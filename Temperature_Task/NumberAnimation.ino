ceef5221f4f232ff02b4f06b04c4fb6cd8e05e0d
Jason C <cruzg@ismanila.org>
2021-05-29 17:19:40 +0400

    Update NumberAnimation.ino

--- Temperature_Task/NumberAnimation.ino
+++ Temperature_Task/NumberAnimation.ino
@@ -10,6 +10,7 @@ extern const unsigned char image_num6B[122];
 extern const unsigned char image_num7B[122];
 extern const unsigned char image_num8B[122];
 extern const unsigned char image_num9B[122];
+extern const unsigned char image_negative[122];
 extern const unsigned char image_degree[152];
 
 void setup()
@@ -19,32 +20,32 @@ void setup()
    
 }
 
-uint8_t FSM = 0;
+uint8_t ctr = 0;
 
 void loop()
 {
-
-  if (FSM < 1)
+  //run the code once
+  if (ctr < 1)
   {
   
   //Show the tens number
   M5.dis.animation((uint8_t *)image_num4B, 150, LED_DisPlay::kMoveLeft, 20);
-  delay(750);
+  delay(750); //length of animation ––DONT CHANGE––
   M5.update();
 
   //Show the ones number
   M5.dis.animation((uint8_t *)image_num3B, 150, LED_DisPlay::kMoveLeft, 20);
-  delay(850);
+  delay(850); //length of animation ––DONT CHANGE––
   M5.update();
 
   //Show the unit (celcius)
   M5.dis.animation((uint8_t *)image_degree, 150, LED_DisPlay::kMoveLeft, 20);
-  delay(1000);
+  delay(1000); //length of animation ––DONT CHANGE––
   M5.update();
 
 
   
-  FSM++;
+  ctr++;
   }
   M5.dis.clear();
   delay(500);
