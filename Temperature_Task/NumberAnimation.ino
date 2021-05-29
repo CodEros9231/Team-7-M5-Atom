09ee177e7cd9fd036cfa98126765610e0c7a54f0
Jason C <cruzg@ismanila.org>
2021-05-29 21:34:52 +0400

    Modular Number Animation
    Able to display any number from -99 to 99

--- Temperature_Task/NumberAnimation.ino
+++ Temperature_Task/NumberAnimation.ino
@@ -1,4 +1,8 @@
+
 #include "M5Atom.h"
+#include "math.h"
+
+bool IMU6886Flag = false;
 
 extern const unsigned char image_num0B[122];
 extern const unsigned char image_num1B[122];
@@ -10,8 +14,10 @@ extern const unsigned char image_num6B[122];
 extern const unsigned char image_num7B[122];
 extern const unsigned char image_num8B[122];
 extern const unsigned char image_num9B[122];
-extern const unsigned char image_negative[122];
 extern const unsigned char image_degree[152];
+extern const unsigned char image_negative[122];
+
+int Temp = -21;
 
 void setup()
 {
@@ -20,32 +26,190 @@ void setup()
    
 }
 
-uint8_t ctr = 0;
+int FSM = 0;
 
 void loop()
 {
-  //run the code once
-  if (ctr < 1)
+
+  uint8_t TempTens = 0;
+  uint8_t TempOnes = 0;
+  
+  if (FSM < 1)
   {
+      if(Temp < 0)
+      {
+            M5.dis.animation((uint8_t *)image_negative, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            Temp = Temp * -1;
+      }
+
+      
+      TempTens = (Temp / 10) % 10;
+      
+      switch(TempTens)
+      {
+        case 0:
+        {
+            break;
+        }
+
+        case 1:
+        {
+            M5.dis.animation((uint8_t *)image_num1B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 2:
+        {
+            M5.dis.animation((uint8_t *)image_num2B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 3:
+        {
+            M5.dis.animation((uint8_t *)image_num3B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 4:
+        {
+            M5.dis.animation((uint8_t *)image_num4B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 5:
+        {
+            M5.dis.animation((uint8_t *)image_num5B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 6:
+        {
+            M5.dis.animation((uint8_t *)image_num6B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 7:
+        {
+            M5.dis.animation((uint8_t *)image_num7B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 8:
+        {
+            M5.dis.animation((uint8_t *)image_num8B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+        case 9:
+        {
+            M5.dis.animation((uint8_t *)image_num9B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(750);
+            M5.update();
+            break;
+        }
+      }
+
+      TempOnes = Temp - (TempTens * 10);
+      
+      switch(TempOnes)
+      {
+        case 0:
+        {
+            M5.dis.animation((uint8_t *)image_num0B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+
+        case 1:
+        {
+            M5.dis.animation((uint8_t *)image_num1B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 2:
+        {
+            M5.dis.animation((uint8_t *)image_num2B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 3:
+        {
+            M5.dis.animation((uint8_t *)image_num3B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 4:
+        {
+            M5.dis.animation((uint8_t *)image_num4B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 5:
+        {
+            M5.dis.animation((uint8_t *)image_num5B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 6:
+        {
+            M5.dis.animation((uint8_t *)image_num6B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 7:
+        {
+            M5.dis.animation((uint8_t *)image_num7B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 8:
+        {
+            M5.dis.animation((uint8_t *)image_num8B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+        case 9:
+        {
+            M5.dis.animation((uint8_t *)image_num9B, 150, LED_DisPlay::kMoveLeft, 20);
+            delay(850);
+            M5.update();
+            break;
+        }
+      }
+
+      
+//      M5.dis.animation((uint8_t *)image_num4B, 150, LED_DisPlay::kMoveLeft, 20);
+//      delay(750);
+//      M5.update();
+//      M5.dis.animation((uint8_t *)image_num3B, 150, LED_DisPlay::kMoveLeft, 20);
+//      delay(850);
+//      M5.update();
+      M5.dis.animation((uint8_t *)image_degree, 150, LED_DisPlay::kMoveLeft, 20);
+      delay(1000);
+      M5.update();
+
+
   
-  //Show the tens number
-  M5.dis.animation((uint8_t *)image_num4B, 150, LED_DisPlay::kMoveLeft, 20);
-  delay(750); //length of animation ––DONT CHANGE––
-  M5.update();
-
-  //Show the ones number
-  M5.dis.animation((uint8_t *)image_num3B, 150, LED_DisPlay::kMoveLeft, 20);
-  delay(850); //length of animation ––DONT CHANGE––
-  M5.update();
-
-  //Show the unit (celcius)
-  M5.dis.animation((uint8_t *)image_degree, 150, LED_DisPlay::kMoveLeft, 20);
-  delay(1000); //length of animation ––DONT CHANGE––
-  M5.update();
-
-
-  
-  ctr++;
+  FSM++;
   }
   M5.dis.clear();
   delay(500);
